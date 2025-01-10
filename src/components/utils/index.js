bea642d1632e592c375c92ffeee6014d3f03cb03
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-10 16:28:30 +0530

    Local Storage

---
+++ src/components/utils/index.js
@@ -0,0 +1,29 @@
+export class LocalStorage {
+    static get(key) {
+        if (!isBrowser) return;
+        const value = localStorage.getItem(key);
+        if (value) {
+            try {
+                return JSON.parse(value);
+            } catch (err) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    static set(key, value) {
+        if (!isBrowser) return;
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    static remove(key) {
+        if (!isBrowser) return;
+        localStorage.removeItem(key);
+    }
+
+    static clear() {
+        if (!isBrowser) return;
+        localStorage.clear();
+    }
+}
