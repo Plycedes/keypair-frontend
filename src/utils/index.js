6b209738a648f5b8b18f1a00251a9895195622bc
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-19 23:42:06 +0530

    Logging error

--- src/utils/index.js
+++ src/utils/index.js
@@ -40,6 +40,7 @@ export const requestHandler = async (api, setLoading, onSuccess, onError) => {
             localStorage.clear();
             window.location.href = "/login";
         }
+        console.log(error);
         onError(error?.response?.data?.message || "Something went wrong");
     } finally {
         setLoading && setLoading(false);
